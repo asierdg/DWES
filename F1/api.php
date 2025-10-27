d5e5f1cda660a081950d1d1ea29b7f7ae48929c8
Asier Díez <asier.diez06@somo.eus>
2025-10-27 09:11:19 +0100

    Orden de atributos mejorado en la llamada a la api

--- F1/api.php
+++ F1/api.php
@@ -25,17 +25,17 @@ if ($err) {
 	$objeto = json_decode($response);
 
 	echo "<b>Informacion del piloto de F1:</b><br><br>";
+    echo "<b>Numero de piloto: </b>" . $objeto[0]->driver_number . "<br>";
 	echo "<b>Nombre de transmision: </b>" . $objeto[0]->broadcast_name . "<br>";
+    echo "<b>Nombre acronimo: </b>" . $objeto[0]->name_acronym . "<br>";
 	echo "<b>Codigo de pais: </b>" . $objeto[0]->country_code . "<br>";
-	echo "<b>Numero de piloto: </b>" . $objeto[0]->driver_number . "<br>";
 	echo "<b>Nombre: </b>" . $objeto[0]->first_name . "<br>";
+    echo "<b>Apellido: </b>" . $objeto[0]->last_name . "<br>";
 	echo "<b>Nombre completo: </b>" . $objeto[0]->full_name . "<br>";
 	echo "<b>Url de foto: </b>" . $objeto[0]->headshot_url . "<br>";
-	echo "<b>Apellido: </b>" . $objeto[0]->last_name . "<br>";
-	echo "<b>Clave de reunion: </b>" . $objeto[0]->meeting_key . "<br>";
-	echo "<b>Nombre acronimo: </b>" . $objeto[0]->name_acronym . "<br>";
-	echo "<b>Clave de sesion: </b>" . $objeto[0]->session_key . "<br>";
+    echo "<b>Nombre del equipo: </b>" . $objeto[0]->team_name . "<br>";
 	echo "<b>Color del equipo: </b>" . $objeto[0]->team_colour . "<br>";
-	echo "<b>Nombre del equipo: </b>" . $objeto[0]->team_name . "<br>";
+    echo "<b>Clave de reunion: </b>" . $objeto[0]->meeting_key . "<br>";
+    echo "<b>Clave de sesion: </b>" . $objeto[0]->session_key . "<br>";
 }
 ?>
